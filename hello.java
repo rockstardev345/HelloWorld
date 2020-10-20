54fec9a4d56479bbbad0e45134b6d2a4d88f856b
Dev <65466984+rockstardev345@users.noreply.github.com>
2020-10-20 19:00:50 -0400

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class Hello{
     public static void main(String[] args){
-        for(int i=2;i<=30;i++){
+        for(int i=2;i<=10;i++){
             System.out.println("Hello World");
 
         }
